e5401530c1493923849a8f9e8c189b29e94f4c1e
Michael Standen <screaminghawk@gmail.com>
2021-10-17 20:53:26 +1300

    Add links to metadata desc

--- generate_metadata.py
+++ generate_metadata.py
@@ -3,34 +3,34 @@ import os
 import random
 
 METADATA = json.dumps({
-	"description": "SpeedrunAI utilizes state of the art machine learning algorithms to generate modern art.", #FIXME Add image and video link to description
-  "external_url": "https://speedrunai.nft",
-  "image": "ipfs://IPFS_LINK/SUBJECT.png",
+	"description": "SpeedrunAI utilizes state of the art machine learning algorithms to generate modern art.\n\n[Image (.png)](ipfs://IPFS_LINK/SUBJECT.png)\n\n[Video (.mp4)](ipfs://IPFS_LINK/SUBJECT.mp4)",
+	"external_url": "https://speedrunai.nft",
+	"image": "ipfs://IPFS_LINK/SUBJECT.png",
 	"animation_url": "ipfs://IPFS_LINK/SUBJECT.mp4",
-  "name": "SUBJECT",
-  "attributes": [
-    {
-      "trait_type": "Subject",
-      "value": "SUBJECT"
-    },
-    {
-      "trait_type": "Style",
-      "value": "STYLE"
-    },
-    {
-      "trait_type": "Iterations",
-      "value": 999
-    },
-    {
-      "trait_type": "Size",
-      "value": "SIZE"
-    },
-    {
-      "trait_type": "Series",
+	"name": "SUBJECT",
+	"attributes": [
+		{
+			"trait_type": "Subject",
+			"value": "SUBJECT"
+		},
+		{
+			"trait_type": "Style",
+			"value": "STYLE"
+		},
+		{
+			"trait_type": "Iterations",
+			"value": 999
+		},
+		{
+			"trait_type": "Size",
+			"value": "SIZE"
+		},
+		{
+			"trait_type": "Series",
 			"display_type": "number",
-      "value": "1"
-    }
-  ]
+			"value": "1"
+		}
+	]
 })
 
 fnames = [f for f in os.listdir('outputs') if '.txt' in f]
