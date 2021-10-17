89ac851f959baabf749b02693aaa711c375ea148
Michael Standen <screaminghawk@gmail.com>
2021-10-17 19:42:48 +1300

    Add generate metadata script

---
+++ generate_metadata.py
@@ -0,0 +1,49 @@
+import json
+import os
+import random
+
+METADATA = json.dumps({
+	"description": "SpeedrunAI utilizes state of the art machine learning algorithms to generate modern art.", #FIXME Add image and video link to description
+  "external_url": "TBA", #FIXME Put something here
+  "image": "ipfs://IPFS_LINK/SUBJECT.png",
+	"animation_url": "ipfs://IPFS_LINK/SUBJECT.mp4",
+  "name": "SUBJECT",
+  "attributes": [
+    {
+      "trait_type": "Subject",
+      "value": "SUBJECT"
+    },
+    {
+      "trait_type": "Style",
+      "value": "STYLE"
+    },
+    {
+      "trait_type": "Iterations",
+      "value": 999
+    },
+    {
+      "trait_type": "Size",
+      "value": "SIZE"
+    },
+    {
+      "trait_type": "Series",
+			"display_type": "number",
+      "value": "1"
+    }
+  ]
+})
+
+fnames = [f for f in os.listdir('outputs') if '.txt' in f]
+random.shuffle(fnames)
+
+for i, fname in enumerate(fnames):
+	with open(f'outputs/{fname}', 'r') as fin:
+		data = fin.readline().strip().split(',')
+		with open(f"metadata/{i}.json", 'w') as fout:
+			fout.write(METADATA
+				.replace('SUBJECT', data[0])
+				.replace('STYLE', data[1])
+				.replace("999", data[2])
+				.replace('SIZE', data[3])
+				.replace('IPFS_LINK', 'QmQGdpRrB8D7MsjazQo2T5jvky7BnZuLpao561M3EynJtS') #FIXME Correct link
+			)
